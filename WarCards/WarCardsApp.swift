1f4e53d54b51eab9da82c2cceb7e7edace295ec2
TDanielsSkidmore <Tdaniels@skidmore.edu>
2022-10-13 13:15:37 -0400

    First version being added

---
+++ WarCards/WarCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardsApp.swift
+//  WarCards
+//
+//  Created by Troy Daniels on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WarCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
